f0ac169dc36aca899f5b216449a9d4e6833688d0
Vladimir Vranceanu <vsvladimir93@gmail.com>
2021-07-14 17:28:09 +0300

    Change MyBatis mapping approach from annotation to XML mappers

--- src/main/java/com/petproject/TelegramBotApi/repository/mapper/KeywordMapper.java
+++ src/main/java/com/petproject/TelegramBotApi/repository/mapper/KeywordMapper.java
@@ -1,23 +1,17 @@
 package com.petproject.TelegramBotApi.repository.mapper;
 
 import com.petproject.TelegramBotApi.repository.dto.Keyword;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface KeywordMapper {
 
-    @Select("SELECT * FROM Keywords")
     List<Keyword> findAll();
 
-    @Insert("INSERT INTO Keywords (keyword) VALUES (#{keyword})")
-    Integer create(Keyword keyword);
+    void create(Keyword keyword);
 
-    @Delete("DELETE FROM Keywords WHERE keyword = #{keyword}")
     Integer delete(String keyword);
 
 }
